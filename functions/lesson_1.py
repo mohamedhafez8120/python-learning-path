26f199b02c9d374f5a1b38e95d8b2867a4f106df
mohamedhafez8120 <mohamed.h.vicekeeper@gmail.com>
2026-02-13 23:31:03 +0200

    lesson_1_added

---
+++ functions/lesson_1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+def get_title(first_name, last_name, job):
+    title1 = first_name + " " + last_name + " the " + job
+    return title1
+
+
+# Don't touch below this line
+
+
+def test(first_name, last_name, job):
+    title = get_title(first_name, last_name, job)
+    print("First name:", first_name)
+    print("Last name:", last_name)
+    print("Job:", job)
+    print("Title:", title)
+    print("=====================================")
+
+
+test("Frodo", "Baggins", "warrior")
+test("Bilbo", "Baggins", "thief")
+test("Gandalf", "The Grey", "wizard")
+test("Aragorn", "Son of Arathorn", "ranger")
